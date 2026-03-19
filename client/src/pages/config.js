94f717d08aa4df917c9bdfbaa6b29c582d484552
dheeraj-patidar-07 <dheerajpatidar802@gmail.com>
2026-03-19 19:48:32 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,8 +1,7 @@
 //const API_BASE_URL = "/api";  // For reverse proxy it is mandatory so dont change
 //const API_BASE_URL = "http://837563944836.realhandsonlabs.net"
 
- const API_BASE_URL = "http://a19dd0a4ee9714384848d2f4865c9a17-206088177.eu-north-1.elb.amazonaws.com
-"
+ const API_BASE_URL = "http://a19dd0a4ee9714384848d2f4865c9a17-206088177.eu-north-1.elb.amazonaws.com"
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 //const API_BASE_URL = "REACT_APP_API_BASE_URL_PLACEHOLDER";
